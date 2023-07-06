c53a858068bf2e6f51e0e0be7c94c21e7f542056
masudpro121 <masud.pro.121@gmail.com>
2023-07-06 09:03:51 +0600

    Layout done

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Logo from '@/assets/img/logo.svg'
 import Image from "next/image"
+import Link from "next/link"
 function Navbar() {
   return (
     <div className="bg-dark flex justify-between py-4 px-7">
@@ -9,7 +10,7 @@ function Navbar() {
       </div>
       <div className="flex gap-5">
           <div>Home</div>
-          <div>Generate</div>
+          <Link href="/pricing">Pricing</Link>
           <div>History</div>
           <div>likes</div>
           <div>Account</div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,34 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import Navbar from "@/components/Navbar/Navbar"
-import Generate from "@/components/Generate/Generate"
-import { createContext, useState } from "react"
-import ShowImages from "@/components/ShowImages/ShowImages"
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import Navbar from "@/components/Navbar/Navbar";
+import Generate from "@/components/Generate/Generate";
+import { createContext, useState } from "react";
+import ShowImages from "@/components/ShowImages/ShowImages";
 
-const inter = Inter({ subsets: ['latin'] })
-export const MyContext = createContext()
+const inter = Inter({ subsets: ["latin"] });
+export const MyContext = createContext();
 
 export default function Home() {
-  const [generatedImage, setGeneratedImage] = useState({})
+  const [generatedImage, setGeneratedImage] = useState({});
   const [inprogress, setInprogress] = useState(false);
-  const [dimension, setDimension] = useState(0)
-  const [sample, setSample] = useState(4)
+  const [dimension, setDimension] = useState(0);
+  const [sample, setSample] = useState(4);
   const value = {
-   generatedImage, setGeneratedImage,
-   dimension, setDimension, 
-   sample, setSample,
-   inprogress,setInprogress
-  }
+    generatedImage,
+    setGeneratedImage,
+    dimension,
+    setDimension,
+    sample,
+    setSample,
+    inprogress,
+    setInprogress,
+  };
   return (
     <MyContext.Provider value={value}>
-      <div className="bg-light text-white min-h-screen">
-      <Navbar />
-      <Generate />
-      <ShowImages />
-    </div>
+      <div >
+        <Generate />
+        <ShowImages />
+      </div>
     </MyContext.Provider>
-  )
+  );
 }
